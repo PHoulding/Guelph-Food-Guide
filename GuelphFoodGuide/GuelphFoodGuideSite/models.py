821b206241b18efe2ea98a3e3a1898d645a24f7a
Unknown <phouldin@uoguelph.ca>
2017-10-17 21:25:04 -0400

    Added dietary restrictions and price point

--- GuelphFoodGuide/GuelphFoodGuideSite/models.py
+++ GuelphFoodGuide/GuelphFoodGuideSite/models.py
@@ -5,9 +5,12 @@ from django.utils.encoding import python_2_unicode_compatible
 # Create your models here.
 @python_2_unicode_compatible
 class Resource(models.Model):
+	price_point = (('$','$'),('$$','$$'),('$$$','$$$'))
 	restaurantName = models.CharField(max_length = 50)
 	restaurantLocation = models.CharField(max_length = 50)
 	restaurantMenu = models.URLField(max_length = 500)
+	dietaryRestrictions = models.CharField(max_length = 50, default="n/a")
+	pricePoint = models.CharField(max_length=3, choices=price_point, default="$$")
 	def __str__(self):
 		return self.restaurantName+", "+self.restaurantLocation+", "+self.restaurantMenu
 
